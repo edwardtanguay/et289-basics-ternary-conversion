445b8513ff8d61fc4c9aa50504e59917d4aa88b3
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-12 13:43:17 +0100

    ternarys finished

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,13 @@ const processingIsFinished = true;
 // }
 const reportStatus = processingIsFinished ? 'finished' : 'processing...';
 
+// string literals (added to JavaScript in 2015 with ES6)
+
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h2>Basics</h2>
 <p>Report: ${reportStatus}</p>
 <p>Next todo: ${reportStatus === 'finished' ? 'call DHL' : 'wait until report is finished'}</p>
+<p>${reportStatus==='finished'? 'DHL Number: +49 22 22 2 2':'' }</p>
 `
