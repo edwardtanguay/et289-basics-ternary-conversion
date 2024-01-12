ae3ba050a7a53abf2c5af62b7592a944792a78e2
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-12 14:11:09 +0100

    certificate message

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 // ternary
 // if statement
 
-const processingIsFinished = true;
+const processingIsFinished = false;
 
 // let reportStatus = '';
 // if (processingIsFinished) {
@@ -16,7 +16,7 @@ const reportStatus = processingIsFinished ? 'finished' : 'processing...';
 // string literals (added to JavaScript in 2015 with ES6)
 // class levels: beginner, intermediate, advanced
 let dataConversionApproved = true;
-let classLevel = 'intermediate';
+let classLevel = 'advanced';
 const topic = 'JavaScript';
 const topic1 = 'ternary';
 const topic2 = 'data conversion';
@@ -26,6 +26,7 @@ const topicLine = `${topic} Basics (${topic1}${classLevel === 'advanced' && data
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h2>${topicLine}</h2>
+  <p>${classLevel === "intermediate" || classLevel === "advanced"? "Please pickup certifacates at office." : ""}</p>
   <p>Report: ${reportStatus}</p>
   <p>Next todo: ${reportStatus === 'finished' ? 'call DHL' : 'wait until report is finished'}</p>
   <p>${reportStatus==='finished'? 'DHL Number: +49 22 22 2 2':'' }</p>
